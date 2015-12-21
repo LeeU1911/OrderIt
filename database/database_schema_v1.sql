ecc3eccd5166bc0fdec756715d23690dd1753e97
Le Tan Vu <imleeu@gmail.com>
2015-12-21 21:12:53 +0700

    Update sql script

--- database/database_schema_v1.sql
+++ database/database_schema_v1.sql
@@ -1,61 +1,68 @@
 CREATE TABLE users (
-	id integer,
+	id integer NOT NULL,
 	username text,
 	password varchar,
 	business_id integer,
 	role_id integer,
 	created datetime,
-	modified datetime
+	modified datetime,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE businesses (
-	id integer,
+	id integer NOT NULL,
 	name varchar,
 	address text,
 	created datetime,
-	modified datetime
+	modified datetime,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE tables (
-	id integer,
+	id integer NOT NULL,
 	business_id integer,
 	name varchar,
 	created datetime,
-	modified datetime
+	modified datetime,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-	id integer,
+	id integer NOT NULL,
 	name varchar,
 	type varchar,
 	created datetime,
-	modified datetime
+	modified datetime,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE orders (
-	id integer,
+	id integer NOT NULL,
 	user_id integer,
 	table_id integer,
 	item_id integer,
 	item_quantity integer,
 	status integer,
 	created datetime,
-	modified datetime
+	modified datetime,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE items (
-	id integer,
+	id integer NOT NULL,
 	name varchar,
 	average_make_time integer,
 	average_make_time_unit varchar,
 	created datetime,
-	modified datetime
+	modified datetime,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE order_status (
-	id integer,
+	id integer NOT NULL,
 	name varchar,
 	created datetime,
-	modified datetime
+	modified datetime,
+	PRIMARY KEY (id)
 );
 
