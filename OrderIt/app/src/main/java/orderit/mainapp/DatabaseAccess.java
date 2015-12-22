1891a939e7b47d3dcb5c1fe16396878f277554d4
NguyenHieuThien <Blackcool@Nguyens-MacBook-Pro.local>
2015-12-22 20:38:49 +0700

    Source code

---
+++ OrderIt/app/src/main/java/orderit/mainapp/DatabaseAccess.java
@@ -0,0 +1,73 @@
+/** History: Created by Hieu Thien
+ * Description: Database access class
+ */
+
+package orderit.mainapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private static DatabaseAccess instance;
+
+    /**
+     * Private constructor to avoid object creation from outside classes.
+     *
+     * @param context
+     */
+    private DatabaseAccess(Context context) {
+        this.openHelper = new DatabaseOpenHelper(context);
+    }
+
+    /**
+     * Return a singleton instance of DatabaseAccess.
+     *
+     * @param context the Context
+     * @return the instance of DabaseAccess
+     */
+    public static DatabaseAccess getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    /**
+     * Open the database connection.
+     */
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    /**
+     * Close the database connection.
+     */
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    /**
+     * Read table name from the database.
+     *
+     * @return a list of table name
+     */
+    public ArrayList<String> getTableList() {
+        ArrayList<String> list = new ArrayList<String>();
+        Cursor cursor = database.rawQuery("SELECT name FROM tables", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+}
---
+++ OrderIt/app/src/main/java/orderit/mainapp/TableList.java
@@ -0,0 +1,58 @@
+/** History: Created by Hieu Thien
+ * Description: Table list activity class
+ */
+
+package orderit.mainapp;
+
+import android.app.Activity;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import android.view.View;
+import android.os.Bundle;
+
+public class TableList extends Activity {
+
+    /**
+     * Variables
+     */
+    private ArrayList<String> arrayTableName = new ArrayList<String>(); /** array of table name */
+
+    /**
+     * Method of creating
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_list);
+
+        /** Query table list from database and assign to array */
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        arrayTableName = databaseAccess.getTableList();
+        databaseAccess.close();
+
+        /** Get table list by ID */
+        ListView tablelist = (ListView) findViewById(R.id.TableList);
+
+        /** Data from data source(database) is assigned to ArrayAdapter */
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, arrayTableName);
+        /** ArrayAdapter is set to ListView */
+        tablelist.setAdapter(adapter);
+
+        /** Clicking each item of list view event */
+        tablelist.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> arg0,
+                                            View arg1,
+                                            int arg2,
+                                            long arg3){
+
+                    }
+                }
+        );
+    }
+
+}
