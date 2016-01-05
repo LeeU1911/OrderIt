17da27e4559ddfa5abc4693b741a8f857bbc138e
Le Tan Vu <imleeu@gmail.com>
2016-01-05 23:52:48 +0700

    Adjust table order layout

--- OrderIt/app/src/main/java/orderit/mainapp/activity/TableOrderActivity.java
+++ OrderIt/app/src/main/java/orderit/mainapp/activity/TableOrderActivity.java
@@ -3,13 +3,16 @@ package orderit.mainapp.activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Space;
@@ -17,12 +20,15 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 import orderit.mainapp.R;
 
 public class TableOrderActivity extends AppCompatActivity {
 
+    private static final int ITEMS_LAYOUT = 999;
     private int cancelBtnImg = R.drawable.close;
     private AtomicInteger no = new AtomicInteger(1);
     private static final int ONE_LINE = 1;
@@ -60,32 +66,35 @@ public class TableOrderActivity extends AppCompatActivity {
 
     private void addRow(View view){
         //add new order row
-        TableLayout ll = (TableLayout) findViewById(R.id.tblLayout);
-        TableRow row = new TableRow(view.getContext());
-        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
-        row.setLayoutParams(lp);
+        GridLayout orderItems = (GridLayout) findViewById(R.id.orderItems);
         TextView no = createSequenceLabel(view.getContext());
-        row.addView(no);
+        orderItems.addView(no);
         EditText code = createTextInput(view.getContext());
         code.setInputType(InputType.TYPE_CLASS_NUMBER);
-        code.setMaxLines(ONE_LINE);
-        row.addView(createSpace(view.getContext()));
-        row.addView(code);
-        TextView item = createTextInput(view.getContext());
-        item.setMaxLines(ONE_LINE);
-        row.addView(createSpace(view.getContext()));
-        row.addView(item);
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+        params.columnSpec = GridLayout.spec(1, 2);
+        params.setGravity(Gravity.FILL);
+        code.setLayoutParams(params);
+        orderItems.addView(code);
+        TextView itemName = createTextInput(view.getContext());
+        GridLayout.LayoutParams params1 = new GridLayout.LayoutParams();
+        params1.columnSpec = GridLayout.spec(3, 3);
+        params1.setGravity(Gravity.FILL);
+        itemName.setLayoutParams(params1);
+        orderItems.addView(itemName);
         TextView quantity = createTextInput(view.getContext());
-        quantity.setMaxLines(ONE_LINE);
-        row.addView(createSpace(view.getContext()));
-        row.addView(quantity);
-        ImageButton cancel = createCancelImgBtn(row);
-        row.addView(cancel);
-        ll.addView(row);
+        orderItems.addView(quantity);
+        orderItems.addView(createTextInput(view.getContext()));
     }
 
     private TextView createLabel(Context context){
-        return new TextView(context);
+        TextView label =  new TextView(context);
+        if (Build.VERSION.SDK_INT < 23) {
+            label.setTextAppearance(context, android.R.style.TextAppearance_Medium);
+        }else{
+            label.setTextAppearance(android.R.style.TextAppearance_Medium);
+        }
+        return label;
     }
     private TextView createSequenceLabel(Context context){
         TextView lbl = createLabel(context);
@@ -94,7 +103,9 @@ public class TableOrderActivity extends AppCompatActivity {
     }
 
     private EditText createTextInput(Context context){
-        return new EditText(context);
+        EditText input = new EditText(context);
+        input.setLines(ONE_LINE);
+        return input;
     }
 
     private Space createSpace(Context context){
@@ -102,11 +113,6 @@ public class TableOrderActivity extends AppCompatActivity {
     }
     private ImageButton createCancelImgBtn(View row) {
         ImageButton cancelBtn = prepareImgBtn(this.cancelBtnImg);
-        TableRow.LayoutParams params = (TableRow.LayoutParams) row.getLayoutParams();
-        params.width = TableRow.LayoutParams.MATCH_PARENT;
-        params.height = TableRow.LayoutParams.MATCH_PARENT;
-
-        cancelBtn.setLayoutParams(params);
         return cancelBtn;
     }
 
