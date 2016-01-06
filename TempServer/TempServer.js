420dcc4e539abfbe7f8a84f4043f34341347b10a
NguyenHieuThien <thien.nguyenhieu@gmail.com>
2016-01-06 22:58:16 +0700

    test

--- TempServer/TempServer.js
+++ TempServer/TempServer.js
@@ -7,7 +7,7 @@ const PORT= process.env.PORT || 8080;
 //We need a function which handles requests and send response
 function handleRequest(request, response){
 	console.log(“request is “, request);
-    response.end('{"response":"success"}');
+    response.end('{"response1”:”success"}');
 }
 
 //Create a server
