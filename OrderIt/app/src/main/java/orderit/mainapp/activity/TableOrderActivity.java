c903d318d67f492273ba2f091f890a8e25f45042
Le Tan Vu <imleeu@gmail.com>
2016-01-09 11:16:30 +0700

    Table order UI #hiBnJW9O

--- OrderIt/app/src/main/java/orderit/mainapp/activity/TableOrderActivity.java
+++ OrderIt/app/src/main/java/orderit/mainapp/activity/TableOrderActivity.java
@@ -1,26 +1,24 @@
 package orderit.mainapp.activity;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
+import android.widget.NumberPicker;
 import android.widget.Space;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.Spinner;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -32,6 +30,7 @@ public class TableOrderActivity extends AppCompatActivity {
     private int cancelBtnImg = R.drawable.close;
     private AtomicInteger no = new AtomicInteger(1);
     private static final int ONE_LINE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,59 +57,93 @@ public class TableOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_table_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        for(int i = 0; i < 10; i++){
-            addRow(findViewById(R.id.tableOrder));
-        }
+        addItemOrders();
 
     }
 
-    private void addRow(View view){
+    private void addItemOrders() {
+        GridLayout orderDetails = (GridLayout) findViewById(R.id.orderDetails);
+        for (int i = 0; i < 10; i++) {
+            addRow(orderDetails);
+        }
+    }
+
+    private void addRow(View view) {
         //add new order row
         GridLayout orderItems = (GridLayout) findViewById(R.id.orderItems);
         TextView no = createSequenceLabel(view.getContext());
         orderItems.addView(no);
         EditText code = createTextInput(view.getContext());
         code.setInputType(InputType.TYPE_CLASS_NUMBER);
+        code.setWidth(getPixelFromDP(60));
         GridLayout.LayoutParams params = new GridLayout.LayoutParams();
         params.columnSpec = GridLayout.spec(1, 2);
         params.setGravity(Gravity.FILL);
         code.setLayoutParams(params);
         orderItems.addView(code);
         TextView itemName = createTextInput(view.getContext());
+        itemName.setWidth(getPixelFromDP(120));
         GridLayout.LayoutParams params1 = new GridLayout.LayoutParams();
         params1.columnSpec = GridLayout.spec(3, 3);
         params1.setGravity(Gravity.FILL);
         itemName.setLayoutParams(params1);
         orderItems.addView(itemName);
-        TextView quantity = createTextInput(view.getContext());
+        Spinner quantity = getQuantitySpinner(view.getContext());
+        quantity.setDropDownWidth(getPixelFromDP(25));
         orderItems.addView(quantity);
-        orderItems.addView(createTextInput(view.getContext()));
+        Button cancel = getCancelButton(view.getContext());
+        orderItems.addView(cancel);
     }
 
-    private TextView createLabel(Context context){
-        TextView label =  new TextView(context);
+    private Button getCancelButton(Context context){
+        Button cancel = new Button(context, null, android.R.attr.buttonStyleSmall);
+        cancel.setText("X");
+        return cancel;
+    }
+    private Spinner getQuantitySpinner(Context context) {
+        Spinner quantity = new Spinner(context);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.qty_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        quantity.setAdapter(adapter);
+        return quantity;
+    }
+
+    private int getPixelFromDP(int dps) {
+//        final float scale = getApplicationContext().getResources().getDisplayMetrics().density;
+//        int pixels = (int) (dps * scale + 0.5f);
+        int pixels = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dps, getResources().getDisplayMetrics());
+        return pixels;
+    }
+
+    private TextView createLabel(Context context) {
+        TextView label = new TextView(context);
         if (Build.VERSION.SDK_INT < 23) {
             label.setTextAppearance(context, android.R.style.TextAppearance_Medium);
-        }else{
+        } else {
             label.setTextAppearance(android.R.style.TextAppearance_Medium);
         }
         return label;
     }
-    private TextView createSequenceLabel(Context context){
+
+    private TextView createSequenceLabel(Context context) {
         TextView lbl = createLabel(context);
         lbl.setText(no.getAndIncrement() + "");
         return lbl;
     }
 
-    private EditText createTextInput(Context context){
-        EditText input = new EditText(context);
-        input.setLines(ONE_LINE);
+    private EditText createTextInput(Context context) {
+        EditText input = (EditText) getLayoutInflater().inflate(R.layout.edit_text, null);
         return input;
     }
 
-    private Space createSpace(Context context){
+    private Space createSpace(Context context) {
         return new Space(context);
     }
+
     private ImageButton createCancelImgBtn(View row) {
         ImageButton cancelBtn = prepareImgBtn(this.cancelBtnImg);
         return cancelBtn;
