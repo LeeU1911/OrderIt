3ba3dd30b0d22d573b691369b1f7b98d45c05673
Le Tan Vu <imleeu@gmail.com>
2016-01-19 22:58:49 +0700

    1. Update android permission to get network connectivity state 2. Update to TempServer to use express library to process POST request

--- TempServer/TempServer.js
+++ TempServer/TempServer.js
@@ -7,6 +7,14 @@ app.get('/', function (req, res) {
   res.send('{"response":"success"}');
 });
 
+app.post('/', function (req, res) {
+	req.on('data', function(data){
+		console.log(data);
+	});
+	
+  res.send('{"response":"success"}');
+});
+
 app.listen(PORT, function () {
   console.log('Example app listening on port %s!', PORT);
 })
