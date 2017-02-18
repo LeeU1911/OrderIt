f90cf9289b72b6bf61315068ae88eafbcac1390a
Le Tan Vu <vu.letan@bosch-si.com>
2017-02-18 22:26:59 +0800

    register and login modals

--- kitchen-module/routes/index.js
+++ kitchen-module/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 var firebase = require('firebase');
 var config = {
@@ -10,34 +11,62 @@ var config = {
   };
 firebase.initializeApp(config);
 
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
+router.use(function (req, res, next) {
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        console.log("Usage is signed in!");
+    } else {
+        console.log("Usage is NOT signed in!");
+    }
+    next();
 });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Order-It Kitchen' });
 });
 
-var user = firebase.auth().currentUser;
+router.post('/login', function (req, res, next) {
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-if (user) {
-  console.log("Usage is signed in!");
-} else {
-  console.log("Usage is NOT signed in!");
-}
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+});
+router.get('/register', function (req, res) {
+    res.render('register');
+});
+router.post('/register', function (req, res, next) {
+    console.log("Register a new user");
+    var email = req.body.email;
+    var password = req.body.password;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function (result) {
+            console.log(result);
+            res.send("User registered successfully!")
+        })
+        .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        res.render('error', {error: {status: 400, stack: errorCode}, message: errorMessage});
+    });
+});
 
-module.exports = router;
+
+app.use('/', router)
+module.exports = app;
